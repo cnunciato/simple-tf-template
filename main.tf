a61b4dc022a0c14bdd45047b3fd1706c7bdcb933
Christian Nunciato <chris@nunciato.org>
2026-08-01 08:58:08 -0700

    Comment backend/remote things

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
 terraform {
-  backend "remote" {
-    hostname     = "tf.pulumi.com"
-    organization = "cnunciato"
-
-    workspaces {
-      name = "my-infra_dev"
-    }
-  }
+  # backend "remote" {
+  #   hostname     = "tf.pulumi.com"
+  #   organization = "cnunciato"  #
+  #   workspaces {
+  #     name = "my-infra_dev"
+  #   }
+  # }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
     }
   }
 }
@@ -20,19 +18,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
-
 module "s3-bucket" {
-  source  = "tf.pulumi.com/veridian/s3-bucket/aws"
-  version = "0.1.5"
-
+  # source  = "tf.pulumi.com/veridian/s3-bucket/aws"
+  # version = "0.1.5"
+  source = "./modules/s3-bucket"
   bucket_name = "my-infra-example-bucket"
 
   tags = {
     Environment = "dev"
-    ManagedBy   = "terraform"
-    SomeKey     = "some-value"
-    Awesome     = "true"
   }
 }
 
